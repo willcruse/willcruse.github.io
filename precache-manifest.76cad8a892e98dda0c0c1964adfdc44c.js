f70b8083e7915d06c9932bf563d100fa35e0a3f1
Will Cruse <willcruse123@protonmail.com>
2020-03-16 10:14:41 +0000

    Updates

---
+++ precache-manifest.76cad8a892e98dda0c0c1964adfdc44c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956d5a8d12eb405482f7d80cc368e900",
+    "url": "/index.html"
+  },
+  {
+    "revision": "866ecdf06ede0d9e0b0f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "25c9db017ff9cbc2122f",
+    "url": "/static/js/2.9002eaa8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.9002eaa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866ecdf06ede0d9e0b0f",
+    "url": "/static/js/main.830b3111.chunk.js"
+  },
+  {
+    "revision": "113f4ac1700ae98b1335",
+    "url": "/static/js/runtime-main.67357851.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
